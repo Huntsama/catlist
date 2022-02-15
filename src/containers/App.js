afe36e7c006cead5172406760b378fdf8cbb9cc3
Huntsama <jawad.itoube@gmail.com>
2022-02-15 22:11:31 +0100

    change laptop

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,16 +8,10 @@ function App() {
     <div className="App">
       <header className="App-header">        
         <p>
-          Edit <code>./App.js</code> and save to reload.
+         live action
+         
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
